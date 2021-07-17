3681059f1e8f001361d8b300b3958651d2714ea1
twozeronine <amdusias209@gmail.com>
2021-07-17 22:05:26 +0900

    캐시 실습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,34 +6,25 @@ namespace ServerCore
 {
     class Program
     {
-        // 전역 변수는 메인과 쓰레드에서 동시에 접근 가능.
-        static bool _stop = false;
-
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작 !");
-
-            while (_stop == false)
-            { 
-                // 누군가 stop 신호를 해주기를 기다린다.
-            }
-            
-            Console.WriteLine("쓰레드 종료 !");
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
-
-            Thread.Sleep(1000); // 1초 대기
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-            t.Wait(); // 쓰레드가 끝남을 알림.
-            Console.WriteLine("종료 성공");
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x,y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
