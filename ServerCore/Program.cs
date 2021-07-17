aea9fe1498266f6bf16a210c7325bd7c15079488
twozeronine <amdusias209@gmail.com>
2021-07-17 20:34:15 +0900

    Thread Practice

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServerCore
 {
     class Program
     {
+        static void MainThread(object state)
+        {
+            for(int i=0; i<5; i++)
+                 Console.WriteLine("Hello Thread!");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            //for (int i=0; i< 4; i++)
+            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    Thread t = new Thread(MainThread);
+            //    //t.Name = "Test Thread";
+            //    t.IsBackground = true;
+            //    t.Start();
+            //}
+
+            //Console.WriteLine("Waiting For Thread!");
+        
+            //t.Join();
+            //Console.WriteLine("Hello World!");
+            while(true)
+            {
+
+            }
         }
     }
 }
