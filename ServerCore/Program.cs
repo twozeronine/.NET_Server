26804d7148405a7d825ce06d50ce7656e5594178
twozeronine <amdusias209@gmail.com>
2021-07-18 14:57:24 +0900

    멀티 쓰레딩 / 데드락 실습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,38 +4,67 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+               UserManager.TestUser();
+            }
+        }
+
+    }
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
     class Program
     {
         static int number = 0;
+        static object _obj = new object();
 
         static void Thread_1()
         {
-            // atomic = 원자성
-
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                // All or Nothing
-                // 먼저 실행되고 있으면 주도권을 갖고 계속 실행
-                // 그동안 Decrement는 실행되고 있지 않다.
-                Interlocked.Increment(ref number);
-                // number++; 아래와 같음
-                
-
-                //int temp = number; // 0 
-                //temp += 1; // 1 
-                //number = temp; // number = 1
+                SessionManager.Test();
             }
         }
+
+        // 데드락 DeadLock
+
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                Interlocked.Decrement(ref number);
-                //int temp = number; // 0 
-                //temp -= 1; // -1 
-                //number = temp; // number = -1
-
-                //number--;
+                UserManager.Test();
             }
         }
         static void Main(string[] args)
@@ -43,6 +72,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(1000);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
