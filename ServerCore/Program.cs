ff5965b5bccc9a366804220e856fb41d2ce0dc51
twozeronine <amdusias209@gmail.com>
2021-07-18 15:46:46 +0900

    멀티 쓰레딩 / 스핀락 실습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,82 +4,70 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    class SessionManager
+    class SpinLock
     {
-        static object _lock = new object();
+         volatile int _locked = 0;
 
-        public static void TestSession()
+        public void Acquire()
         {
-            lock (_lock)
+            while (true)
             {
+                // version 1
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)
+                //    break;
 
+                // version 2
+                // CAS Compare-And-Swap
+                int expected = 0;
+                int desired = 1;
+                if(Interlocked.CompareExchange(ref _locked, desired, expected)==expected)
+                    break;
             }
         }
 
-        public static void Test()
+        public void Release()
         {
-            lock (_lock)
-            {
-               UserManager.TestUser();
-            }
-        }
-
-    }
-    class UserManager
-    {
-        static object _lock = new object();
-
-        public static void Test()
-        {
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-
-            }
+            _locked = 0;
         }
     }
+
     class Program
     {
-        static int number = 0;
-        static object _obj = new object();
+        static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
         static void Thread_1()
         {
-            for (int i = 0; i < 100; i++)
+            for(int i=0; i<100000; i++)
             {
-                SessionManager.Test();
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+                
             }
         }
-
-        // 데드락 DeadLock
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                UserManager.Test();
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
             }
         }
+
         static void Main(string[] args)
         {
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
-
-            Thread.Sleep(1000);
-
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(_num);
         }
     }
 }
