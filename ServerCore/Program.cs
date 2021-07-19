a212fb6515f2a312bfada34cafc02a0789360193
twozeronine <amdusias209@gmail.com>
2021-07-19 22:30:16 +0900

    멀티 쓰레딩 / ReadWriteLock 실습

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,64 +6,35 @@ namespace ServerCore
 {
     class Program
     {
-        // 1. 근성
-        // 2. 양보
-        // 3. 갑질
-
-        // 상호배제
-        // Monitor
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock(); // 내부 구현으로는 중간에 한번씩 양보함.
-        static Mutex _lock3 = new Mutex();
-
-        // [] [] [] [] [] [] 
-        class Reward
-        {
-
-        }
-
-        // RWLock ReaderWriteLock
-        static ReaderWriterLockSlim _lock4 = new ReaderWriterLockSlim();
-
-        // 99.999999%
-        static Reward GetRewardById(int id)
-        {
-            _lock4.EnterReadLock();
-
-            _lock4.ExitReadLock();
-
-            lock(_lock)
-            {
-
-            }
-            return null;
-        }
-
-        // 0.000001%
-        static void AddReward(Reward reward)
-        {
-            _lock4.EnterWriteLock();
-
-            _lock4.ExitWriteLock();
-        }
-
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
         static void Main(string[] args)
         {
-            lock(_lock)
+            Task t1 = new Task(() =>
+           {
+               for (int i = 0; i < 100000; i++)
+               {
+                   _lock.WriteLock();
+                   count++;
+                   _lock.WriteUnlock();
+               }
+           });
+            Task t2 = new Task(() =>
             {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
 
-            }
+            t1.Start();
+            t2.Start();
 
-            bool lockTaken = false;
-            try
-            {
-                _lock2.Enter(ref lockTaken);
-            }
-            finally
-            {
-                if(lockTaken)
-                _lock2.Exit();
-            }
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(count);
         }
     }
 }
