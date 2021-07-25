6a8af3aa85594879784e433608c8bcc96da6c2b3
twozeronine <amdusias209@gmail.com>
2021-07-25 21:38:58 +0900

    멀티 쓰레딩 / 소켓 프로그래밍 Listener 구현

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace DummyClient
 {
@@ -15,34 +16,38 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            // 휴대폰 설정
-            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            try
+            while(true)
             {
-                // 문지기한테 입장 문의
-                socket.Connect(endPoint);
-                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+                // 휴대폰 설정
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                // 보낸다
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
-                int sendBytes = socket.Send(sendBuff);
+                try
+                {
+                    // 문지기한테 입장 문의
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
 
-                // 받는다 
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = socket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Server] {recvData}");
+                    // 보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    int sendBytes = socket.Send(sendBuff);
 
-                // 나간다
-                socket.Shutdown(SocketShutdown.Both);
+                    // 받는다 
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
 
+                    // 나간다
+                    socket.Shutdown(SocketShutdown.Both);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(1000);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-
         }
     }
 }
---
+++ ServerCore/Listener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            // 문지기 교육
+            _listenSocket.Bind(endPoint);
+
+            // 영업 시작
+            // backlog : 최대 대기수
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            //소켓을 재사용하기 때문에 한번 초기화해줌
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // TODO
+                _onAcceptHandler?.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            // 성공 했으니 다시 새로운 클라이언트를 기다림
+            RegisterAccept(args);
+        }
+    }
+}
